58cb3f5255b1174f4d39708a85a66edd081b6a63
unknown <igna.cwaniak@gmail.con>
2019-12-04 14:54:41 +0100

    Refactor, files, formula in rt Matrix, init for Rotaion prevalue

--- public/main.js
+++ public/main.js
@@ -13,47 +13,22 @@
 var id = 0;
 var COSV = "c"
 var SINV = "s"
-var TIMV = "t"
+var TIMV = {"user": "t", "math":"millis()"}
 var MINUSV = "-"
 // const Rematrix = require('rematrix');
 
 
 
-function createUnitMatrix(i){
-  temp = []
-  for (var x=0;x<i;x++)
-  {
-    for (var y=0;y<i;y++)
-    {
-      temp.push(Number(x==y));
-    }
-  }
-  console.log(temp);
-  return temp;
-}
-
-
 function axis0(){
   this.show = function()
   {
-    push();
-    translate(10,0,0);
-    box(20,5,5);
-    pop();
-    push();
-    translate(0,15,0);
-    box(5,30,5);
-    pop();
-    push();
-    translate(0,0,20);
-    box(5,5,40);
-    pop();
+    push(); translate(10,0,0); box(20,5,5); pop();
+    push(); translate(0,15,0); box(5,30,5); pop();
+    push(); translate(0,0,20); box(5,5,40); pop();
   }
-
   this.dh = function() {
     // rotate
   }
-
 }
 
 
@@ -82,42 +57,20 @@ function part_1(obr1, tran1, tran2, obr2, parent){ // x, y, z
   this.child = null;
   this.ps = 10;
 
-  this.dhTable = null
+  this.dhTable = null;
+
+  this.timeValues = {};
 
   this.show = function() {
+    this.timeUpdate();
     applyMatrix(...this.rtMatrix);
     normalMaterial();
     box(this.ps,this.ps,this.ps*3);
   }
 
-  this.dh = function() {
-    // rotateZ(this.dhArray[0]);
-    // translate(0,0,this.dhArray[1])
-    // translate(this.dhArray[2],0,0)
-    // rotateX(this.dhArray[3]);
-
-    // for (var i = 0; i <4; i++)
-    // {
-    //     for (var j = 0; j <4; j++){
-    //       inputGroupFixed[i*4+j] = inputGroup[i*4+j];
-    //       inputGroupValue[i*4+j] = Number(inputGroup[i*4+j].value());
-    //       inputGroupValueFixed[j*4+i] = Number(inputGroup[i*4+j].value());
-    //     }
-    // }
-    // console.dir(Rematrix.identity().map(a => a));
-    // [a,b,c,d].reduce(Rematrix.multiply)
-    // var asdfg = Rematrix.identity()
-
-    // console.dir([a,b,c,d].reduce(Rematrix.multiply));
-    // return
-  }
   this.rtMatrixUser = function(inp){
-      // var r11, r12, r13, r21, r22, r23, r31, r32, r33, tx,  tz , ty , s;
-      // console.log(inp);
-
        applyMatrix(...inp);
   }
-
 
   this.updateDh = function(newDh){
     switch(arguments.length) {
@@ -131,201 +84,136 @@ function part_1(obr1, tran1, tran2, obr2, parent){ // x, y, z
     var c = Rematrix.translate3d(this.dhArray[2],0,0)
     var d = Rematrix.rotateX(this.dhArray[3]);
     this.rtMatrix = [a,b,c,d].reduce(Rematrix.multiply);
-    applyDataToMatrixRT()
+    // applyDataToMatrixRT()
   }
 
   this.updateRtMatrix = function(inp){
+    console.dir(inp)
     this.rtMatrix = inp;
   }
 
-  //
-  // this.rtTest = function(){
-  //   var r11, r12, r13, r21, r22, r23, r31, r32, r33, tx,  tz , ty , s;
-  //    applyMatrix( r11, r12, r13, 0.0,
-  //                 r21, r22, r23, 0.0,
-  //                 r31, r32, r33, 0.0,
-  //                 tx,  ty , tz , s   );
-  // }
+  this.timeUpdate = function(){
 
+    var parser = new formulaParser.Parser();
+    parser.setVariable("t", millis())
+    for (var pointT in this.timeValues)
+    {
+    var point = this.timeValues[pointT];
+      var a = parser.parse(point.v)
 
-  // this.rt = function(){
-  //  applyMatrix.apply(this, this.rtMatrix);
-  // }
+      this.rtMatrix[point.y*4 + point.x] = a.result;
 
-    // this.createChild = function() {
-  //   var tx = this.x;
-  //   // this.child = part_1();
-  // }
+      // console.dir(this.rtMatrix)
+    }
+  }
 }
+
+// console.log(parser.parse('sin(PI()/2)'));
+
 
 function applyDataToMatrixRT(){
-  var tab = []
-  for (var part in partGroup)
-  {
-    // console.log(part);
-      tab.push(partGroup[part].rtMatrix);
-  }
-
-  tab = tab.reduce(Rematrix.multiply);
-  console.log(tab);
-  for (var i = 0; i <4; i++)
-  {
-      for (var j = 0; j <4; j++){
-          inputGroup[i*4+j].value( (Math.round(Number(tab[j*4+i])).toString()) );
-      }
-  }
+    var tab
+    for (var part in partGroup)
+    {
+      // console.log(part);
+        // tab.push(partGroup[part].rtMatrix);
+        tab = (partGroup[part].rtMatrix) //.reduce(Rematrix.multiply);
+        console.log(tab);
+        for (var i = 0; i <4; i++)
+        {
+            for (var j = 0; j <4; j++){
+                if(Number(tab[j*4+i]))
+                {
+                  rtInput[part][j*4+i].value( (Math.round(Number(tab[j*4+i])*10000 )/ 10000).toString());
+                }
+                else
+                {
+                  rtInput[part][j*4+i].value((tab[j*4+i]).toString());
+                }
+            }
+        }
+    }
 }
 
-function changeValueOfMatrixRT(){
-  console.log("boom")
 
-      // if(inputGroup.radio.value() == 1)
-      //   {
-      //     inputGroup[1*4+1].value(COSV);
-      //     inputGroup[2*4+2].value(COSV);
-      //     inputGroup[1*4+2].value(SINV);
-      //     inputGroup[2*4+1].value(MINUSV + SINV);
-      //   }
 
-      for (var i = 0; i <4; i++)
-      {
-          for (var j = 0; j <4; j++){
-            inputGroupFixed[i*4+j] = inputGroup[i*4+j];
-            inputGroupValue[i*4+j] = Number(inputGroup[i*4+j].value());
-            inputGroupValueFixed[j*4+i] = Number(inputGroup[i*4+j].value());
-          }
-      }
-      if(partGroup.length != 0){
-        partGroup[this.idPart].updateRtMatrix(inputGroupValueFixed);
-
-      }
-
-}
-
-function createMatrixRT(x, y, w, h ){
+function changeValueOfMatrixRT(idT1, idT2){
+    if(typeof(idT1)=="number"){
+      idT = idT2;
+    }
+    else{
+      idT = this.idPart;
+    }
+    console.log(idT);
+    console.dir(this);
 
     for (var i = 0; i <4; i++)
     {
-        for (var j = 0; j <4; j++)
-          {
-            inputGroup[i*4+j] = createInput(((i===j)*1).toString(), "text");//"r" + (i+1).toString() + (j+1).toString());
-            inputGroup[i*4+j].position(x+j*(w+10), y+i*h);
-            inputGroup[i*4+j].style('width', (w).toString() + 'px');
-            inputGroup[i*4+j].input(changeValueOfMatrixRT)
-            inputGroup[i*4+j].idPart = id;
-            // inputGroup[i*4+j].idX = i;
-            // console.log(i*4 + j)
+        for (var j = 0; j <4; j++){
+          if( ( rtInput[idT][i*4+j].value() ).includes(TIMV.user) ){
+            // rtInput[idT][i*4+j].value(1);
+            partGroup[idT].timeValues[i*4+j]= {x:j, y:i, v:rtInput[idT][i*4+j].value()};
           }
+          else{
+          // rtInputFixed[idT][i*4+j] = rtInput[idT][i*4+j];
+          delete partGroup[idT].timeValues[i*4+j];
+          rtInputValue[idT][i*4+j] = Number(rtInput[idT][i*4+j].value());
+          // rtInputValueFixed[idT][j*4+i] = Number(rtInput[idT][i*4+j].value());
+          }
+        }
     }
-    inputGroup.radio = createRadio();
-    inputGroup.radio.position(x+0*(w+10), y+5*h);
-    inputGroup.radio.option('None', 0);
-    inputGroup.radio.option('x', 1);
-    inputGroup.radio.option('y', 2);
-    inputGroup.radio.option('z', 2);
-    inputGroup.radio.style('width', '180px');
-    inputGroup.radio.input(changeValueOfMatrixRT);
 
-    changeValueOfMatrixRT();
-}
-
-function createDh(){
-  dhSize = {
-    x: 60, y: height, w: 30, h: 30, xspace: 40
+  if((partGroup.length != 0)){
+    // console.dir(rtInputValueFixed[idT])
+    partGroup[idT].updateRtMatrix(rtInputValue[idT]);
   }
-
-  textDh[0] = createSpan('Teta');
-  textDh[0].position(dhSize.x + 0*dhSize.xspace , height);
-  textDh[1] = createSpan('d');
-  textDh[1].position(dhSize.x + 1*dhSize.xspace , height);
-  textDh[2] = createSpan('a');
-  textDh[2].position(dhSize.x + 2*dhSize.xspace , height);
-  textDh[3] = createSpan('alfa');
-  textDh[3].position(dhSize.x + 3*dhSize.xspace , height);
 }
 
-function moveAddbutton(){
+function rtButtonPressed(){
+  console.log(this.matrix)
+  switch (this.matrix) {
+    case "None":
+      break;
+    case "x":
+      // rtInput[this.idPart] = createXRotationMatrix();
+      // changeValueOfMatrixRT(-1, this.idPart);
 
-  addButton.position(10, height + 30 + 30*id);
+      // partGroup[this.idPart].updateRtMatrix();
+      // applyDataToMatrixRT();
+      break;
+    case "y":
+      break;
+    case "z":
+      break;
+    default:
+      break;
+
+  }
 }
 
 
 function changeValueOfArrayDh(){
   console.dir(this);
-  dhInputValue[this.idY][this.idX] = this.value();
-  partGroup[this.idY].dhArray[this.idX] = Number(this.value());
-  partGroup[this.idY].updateDh()
-    // for (var i = 0; i <4; i++){
-    //     for (var j = 0; j <4; j++){
-    //     }
-    // }
-}
-
-function addPartArray(){
-
-  dhInput[id] = [];
-  dhInputValue[id] = []
-  for (var i = 0; i <4; i++){
-    dhInput[id][i] = createInput("0");//"r" + (i+1).toString() + (j+1).toString());
-    dhInput[id][i].position(60 + i*dhSize.xspace, height + 30 + 30*id);
-    dhInput[id][i].style('width', 30 + 'px');
-    dhInput[id][i].input(changeValueOfArrayDh)
-    dhInput[id][i].idY = id;
-    dhInput[id][i].idX = i;
-    dhInputValue[id][i] = 0;
+  dhInputValue[this.idPart][this.idX] = this.value();
+  for ( var x in dhInputValue[this.idPart]){
+    if(this.idPart < id){
+      partGroup[this.idPart].dhArray[this.idX] = Number(this.value());
+      partGroup[this.idPart].updateDh()
+    }
   }
 }
 
 function addPart(){
   // moveAddbutton();
   var tempPart = new part_1(dhInputValue[id]);
-  partGroup.push(tempPart); //dhInput[0].value(),dhInput[1].value(),dhInput[2].value(),dhInput[3].value()
-  dhInput[id].part = tempPart;
-  console.dir(dhInput[id].part)
+  partGroup.push(tempPart);
+
+  createMatrixRT(width, 0, 40, 30);
+
+  partGroup[id].updateDh();
   id++;
   moveAddbutton();
-  addPartArray();
-}
-
-function createUI(){
-
-    createMatrixRT(width, 0, 40, 30)
-    console.log(inputGroup);
-
-    addButton = createButton('Add');
-    addButton.position(10, height + 30);
-    addButton.mousePressed(addPart);
-
-    //DH Table
-    createDh();
-    addPartArray();
-
-    radio = createRadio();
-    radio.position(60 + 5*dhSize.xspace, height + 30);
-    radio.option('dh', 1);
-    radio.option('matrix', 2);
-    // radio.sele = 1;
-    radio.style('width', '120px');
-    // console.dir(radio.value())
-    radio._getInputChildrenArray()[0].checked = true;
-    // console.dir(radio.value())
-
-    let a ={"asd" : 1, "sdf" : 2, "dfg" : 3};
-
-    jsonOfPartsIn = createInput();
-    jsonOfPartsIn.position(60 + 8*dhSize.xspace, height + 30);
-    jsonOfPartsIn.style('width', (200).toString() + 'px');
-
-    jsonOfPartsButtonIn = createButton('LoadJson');
-    jsonOfPartsButtonIn.position(60 + 8*dhSize.xspace, height + 60);
-
-    jsonOfPartsOut = createInput(JSON.stringify(a));
-    jsonOfPartsOut.position(60 + 8*dhSize.xspace, height + 90);
-    jsonOfPartsOut.style('width', (200).toString() + 'px');
-
-    jsonOfPartsButtonOut = createButton('GetJson');
-    jsonOfPartsButtonOut.position(60 + 8*dhSize.xspace, height + 120);
-
+  addDhArray();
 }
 
 // SInusy cosinusy, macierze obrotu radio
@@ -344,14 +232,14 @@ function createUI(){
 // o, d, a, alfa
 let sliderGroup = [];
 
-let inputGroup = [];
-let inputGroupFixed = [];
-let inputGroupValue = [];
-let inputGroupValueFixed = [];
+let rtInputFixed = [];
+let rtInputValue = [];
+let rtInputValueFixed = [];
 
 let partGroup = [];
 
 let inputMatrix = {};
+let rtInput = []
 
 let addButton;
 let dhSize;
@@ -375,25 +263,9 @@ function setup() {
   camera(90, -90, 90, 0, 0, 0);
   normalMaterial();
   debugMode();
-  // console.log(a);
 
   createUI();
 
-  // printMatrix()
-  // addButton.mousePressed();
-  // jsonOfParts.input(changeValueOfMatrixRT)
-
-
-  // textFont(myFont);
-
-  // inputGroup[i*4+j].input(changeValueOfMatrixRT);
-
-  // sliderGroup[0] = createSlider(-400, 400, 0);
-  // sliderGroup[0].position(10, height + 90);
-  // sliderGroup[0].style('width', '80px');
-  // sliderGroup[1] = createSlider(-400, 400, 0);
-  // sliderGroup[1].position(10, height + 60);
-  // sliderGroup[1].style('width', '80px');
 }
 
 function draw() {
@@ -403,41 +275,24 @@ function draw() {
   let ct = cos(rad);
   let st = sin(rad);
   orbitControl();
-  // a.obr1 = sliderGroup[0].value();
-  // a2.obr1 = sliderGroup[1].value();
   background(250);
   rotateX(90);
   push();
-  // translate(0, 20, 0);
   a0.show();
-  // a.matrixUser(inputGroupValueFixed);
-  // a.show();
   for (var part in partGroup)
   {
     // console.log(part);
     if(radio.value() == 1){
-      partGroup[part].dh();}
+      // partGroup[part].dh();
+    }
     else{
-      partGroup[part].rt();}
+      partGroup[part].rt();
+    }
       partGroup[part].show()
       a0.show()
   }
-  // a1.dh()
-  // a1.show();
-  // a2.dh()
-  // a2.show();
-
-
-  // applyMatrix.apply(this, inputGroupValue);
-   // console.log(inputGroupValueFixed);
-
-  // rotateX(45);
-  // box(10,20,10);
   // resetMatrix();
   pop();
-  // textSize(32);
-  // fill(0, 102, 153);
-  // textFont(myFont);
-  // text('Teta',0 ,0);
-
 }
+
+// createUnitMatrix
